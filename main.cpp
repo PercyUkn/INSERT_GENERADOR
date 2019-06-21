5c0b5f8dbe1e82cd4518e5b399aa30fa225c83cd
Percy Escalante <percy.escalante.b@uni.pe>
2019-06-21 03:32:52 -0500

    Ahora se puede tratar recursivamente varios archivos
    En el Excel se debe poner los atributos entre comillas simples ( ' ' )

--- main.cpp
+++ main.cpp
@@ -3,34 +3,52 @@
 #include <sstream>
 #include <string>
 #include <algorithm>
+#include <stdio.h>
+#include <ctype.h>
 
 using namespace std;
 
 
 int main() {
+	bool flag = true;
+	char resp;
+	while (flag)
+	{
 
-    string direccion,nombreTabla,lineaCSV;
-    cout<<"Ingrese la direccion del archivo: "<<endl;
-    getline(cin,direccion);
-    cout<<"Ingrese nombre de la tabla: "<<endl;
-    getline(cin,nombreTabla);
-    ifstream archivoCSV (direccion.c_str(),ios::in);
-    direccion.replace(direccion.begin()+direccion.length()-3,direccion.end(),"txt");
-    ofstream archivoGenerado (direccion.c_str(),ios::out);
-    int numeroLineas = std::count(std::istreambuf_iterator<char>(archivoCSV),std::istreambuf_iterator<char>(), '\n');
-    cout<<"Numero de Lineas: "<<numeroLineas<<endl;
-    archivoCSV.clear();
-    archivoCSV.seekg(0, ios::beg);
-    while (getline(archivoCSV,lineaCSV))
-    {
 
-        if (numeroLineas>=1)
-            {lineaCSV.pop_back();}
+		string direccion, nombreTabla, lineaCSV;
+		cout << "Ingrese la direccion del archivo: " << endl;
+		getline(cin, direccion);
+		cout << "Ingrese nombre de la tabla: " << endl;
+		getline(cin, nombreTabla);
+		ifstream archivoCSV(direccion.c_str(), ios::in);
+		direccion.replace(direccion.begin() + direccion.length() - 3, direccion.end(), "txt");
+		ofstream archivoGenerado(direccion.c_str(), ios::out);
+		int numeroLineas = std::count(std::istreambuf_iterator<char>(archivoCSV), std::istreambuf_iterator<char>(), '\n');
+		cout << "Numero de Lineas: " << numeroLineas << endl;
+		archivoCSV.clear();
+		archivoCSV.seekg(0, ios::beg);
+		while (getline(archivoCSV, lineaCSV))
+		{
 
-            archivoGenerado<<"INSERT INTO "<<nombreTabla<<" VALUES ("<<lineaCSV<<");"<<endl;
-        numeroLineas--;
-    };
-    archivoGenerado.close();
-    return 0;
+			if (numeroLineas >= 1)
+			{
+				lineaCSV.pop_back();
+			}
+
+			archivoGenerado << "INSERT INTO " << nombreTabla << " VALUES (" << lineaCSV << "');" << endl;
+			numeroLineas--;
+		};
+		cout << "Archivo generado en: " << direccion;
+		archivoGenerado.close();
+		cout << "Continuar? [S/N] ";
+		cin >> resp;
+		cin.ignore();
+		if (toupper(resp) == 'N') 
+		{
+			flag = false;
+		}
+	}
+	return 0;
 
 }
